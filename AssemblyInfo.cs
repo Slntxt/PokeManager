225c1201bc0d0b79ed28f790b0dd5eff65ee8f7f
Slntxt <34522066+Slntxt@users.noreply.github.com>
2017-12-22 16:09:11 -0500

    Update AssemblyInfo.cs

--- AssemblyInfo.cs
+++ AssemblyInfo.cs
@@ -33,6 +33,6 @@
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.0.1")]
 
-[assembly: AssemblyVersion("1.0.0.1")]
-[assembly: AssemblyFileVersion("1.0.0.1")]
-[assembly: AssemblyInformationalVersion("v1.0.0.1")]
+[assembly: AssemblyVersion("1.0.0.2")]
+[assembly: AssemblyFileVersion("1.0.0.2")]
+[assembly: AssemblyInformationalVersion("v1.0.0.2")]
