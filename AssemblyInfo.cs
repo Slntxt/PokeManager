38bdf2863d846d2933e2b94e9191e25ab490eded
Slntxt <34522066+Slntxt@users.noreply.github.com>
2018-02-12 23:52:09 -0500

    Update AssemblyInfo.cs

--- AssemblyInfo.cs
+++ AssemblyInfo.cs
@@ -31,8 +31,8 @@
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.0.11")]
+// [assembly: AssemblyVersion("1.0.0.12")]
 
-[assembly: AssemblyVersion("1.0.0.11")]
-[assembly: AssemblyFileVersion("1.0.0.11")]
-[assembly: AssemblyInformationalVersion("v1.0.0.11")]
+[assembly: AssemblyVersion("1.0.0.12")]
+[assembly: AssemblyFileVersion("1.0.0.12")]
+[assembly: AssemblyInformationalVersion("v1.0.0.12")]
